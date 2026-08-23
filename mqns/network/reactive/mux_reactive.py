68119ecbea3cb4bd935f313e140b9d65f672f28e
Junxiao Shi <git@mail1.yoursunny.com>
2026-08-23 18:06:19 +0000

    reactive: MuxSchemeReactive cleanup

--- mqns/network/reactive/mux_reactive.py
+++ mqns/network/reactive/mux_reactive.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, Literal, Protocol, override
+from typing import override
 
 from mqns.entity.memory import MemoryQubit, PathDirection, QubitState
 from mqns.entity.node import QNode
@@ -7,87 +7,13 @@ from mqns.models.epr import Entanglement
 from mqns.network.fw.fib import FibPath
 from mqns.network.fw.message import PathInstructions, validate_path_instructions
 from mqns.network.fw.mux import MuxScheme
-from mqns.network.fw.select import (
-    MemoryEprIterator,
-    MemoryEprTuple,
-    call_select,
-    parse_select,
-    select_random,
-    select_swap_qubit_newest,
-    select_swap_qubit_oldest,
-)
-from mqns.utils import unwrap_cast
-
-if TYPE_CHECKING:
-    from mqns.network.fw.forwarder import Forwarder
+from mqns.utils import unwrap, unwrap_cast
 
 
-class MuxSchemeFibBase(MuxScheme):
-    class SelectSwapQubit(Protocol):
-        """
-        Function to select a preferred swap candidate.
-
-        When qubit ``mq0`` on channel ``ch0`` becomes ELIGIBLE and the forwarder decides to swap it with a qubit from ``ch1``,
-        but there are multiple candidate qubits from ``ch1``, this function is called to select a qubit.
-        """
-
-        def __call__(
-            self, candidates: list[MemoryEprTuple], fw: "Forwarder", mq0: MemoryEprTuple, fp: FibPath, /
-        ) -> MemoryEprTuple:
-            """
-            Args:
-                candidates: List of qubits from ``ch1``, guaranteed to have two or more items.
-                fw: The forwarder instance.
-                mq0: The qubit from ``ch0``.
-                fp: FIB path entry.
-
-            Returns:
-                One item from ``candidates``.
-            """
-            ...
-
-    type SelectSwapQubitInput = SelectSwapQubit | Literal["random", "oldest", "newest"] | None
-
-    SelectSwapQubit_random: SelectSwapQubit = select_random
+class MuxSchemeReactive(MuxScheme):
     """
-    Select a random qubit among the candidates, following uniform distribution.
+    ``MuxScheme`` adapted for reactive forwarding.
     """
-    SelectSwapQubit_oldest: SelectSwapQubit = select_swap_qubit_oldest
-    """
-    Select the qubit that becomes ELIGIBLE in this forwarder the earliest, i.e. First-In-First-Out (FIFO).
-    """
-    SelectSwapQubit_newest: SelectSwapQubit = select_swap_qubit_newest
-    """
-    Select the qubit that becomes ELIGIBLE in this forwarder the latest, i.e. Last-In-First-Out (LIFO).
-    """
-
-    def __init__(self, select_swap_qubit: SelectSwapQubitInput):
-        super().__init__()
-        self._select_swap_qubit = parse_select(type(self), "SelectSwapQubit_", select_swap_qubit)
-
-    def select_swap_candidate(
-        self, mq0: MemoryQubit, epr0: Entanglement, fp: FibPath, candidates: MemoryEprIterator
-    ) -> tuple[MemoryQubit, FibPath] | None:
-        assert fp
-        mt1 = call_select(candidates, self._select_swap_qubit, self.fw, (mq0, epr0), fp)
-        return None if mt1 is None else (mt1[0], fp)
-
-
-class MuxSchemeReactive(MuxSchemeFibBase):
-    """
-    Buffer-Space multiplexing scheme.
-    """
-
-    def __init__(
-        self,
-        *,
-        select_swap_qubit: MuxSchemeFibBase.SelectSwapQubitInput = None,
-    ):
-        """
-        Args:
-            select_swap_qubit: Function to select a qubit to swap with, default is first.
-        """
-        super().__init__(select_swap_qubit)
 
     @override
     def validate_path_instructions(self, inst: PathInstructions) -> None:
@@ -95,6 +21,8 @@ class MuxSchemeReactive(MuxSchemeFibBase):
 
     @override
     def install_path_adj(self, inst: PathInstructions, fp: FibPath, dir: PathDirection, ch: QuantumChannel) -> None:
+        _ = ch
+
         assert "reactive_qubits" in inst
         idx = fp.own_idx + (-1 if dir == PathDirection.L else 0)
         key = inst["reactive_qubits"][idx]
@@ -110,12 +38,8 @@ class MuxSchemeReactive(MuxSchemeFibBase):
 
     @override
     def uninstall_path_adj(self, fp: FibPath, dir: PathDirection, ch: QuantumChannel) -> None:
-        qubits = self.memory.find(lambda q, _: q.path_id == fp.path_id, qchannel=ch)
-        addrs = [q[0].addr for q in qubits]
-        self.fw.log_debug("deallocating %s qubits: %s", dir, addrs)
-        # If some qubits are currently ACTIVE or RESERVED IN LinkLayer, deallocation would occur
-        # when they next reach RAW state.
-        self.memory.deallocate(*addrs)
+        _ = fp, dir, ch
+        raise ValueError(f"{self} should not receive PATH_DELETE command")
 
     @override
     def qubit_has_path_id(self) -> bool:
@@ -123,7 +47,7 @@ class MuxSchemeReactive(MuxSchemeFibBase):
 
     @override
     def list_qubit_epr_path_ids(self, mq: MemoryQubit) -> list[int]:
-        if mq.path_id is None:  # path has been uninstalled
+        if mq.path_id is None:  # qubit not used in PATH_INSERT
             return []
         return [mq.path_id]
 
@@ -135,17 +59,18 @@ class MuxSchemeReactive(MuxSchemeFibBase):
 
     @override
     def find_swap_with(self, mq0: MemoryQubit, epr0: Entanglement, fp: FibPath | None) -> tuple[MemoryQubit, FibPath] | None:
-        assert fp
-        return self.select_swap_candidate(
-            mq0,
-            epr0,
-            fp,
-            self.memory.find(
-                lambda q, _: (
-                    self.qubits_swappable(mq0, q)  # basic condition met
-                    and q.path_id == mq0.path_id  # allocated to the same path_id
-                    and q.path_direction is not mq0.path_direction  # in the opposite path direction
-                ),
-                has=self.epr_type,
-            ),
-        )
+        _ = epr0
+        try:
+            mq1, _ = next(
+                self.memory.find(
+                    lambda q, _: (
+                        self.qubits_swappable(mq0, q)  # basic condition met
+                        and q.path_id == mq0.path_id  # allocated to the same path_id
+                        and q.path_direction is not mq0.path_direction  # in the opposite path direction
+                    ),
+                    has=self.epr_type,
+                )
+            )
+            return mq1, unwrap(fp)
+        except StopIteration:
+            return None
