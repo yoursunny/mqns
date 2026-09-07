9a787bd3489424d07bedc31d5e93c485ebbe5506
Junxiao Shi <git@mail1.yoursunny.com>
2026-09-07 20:14:08 +0000

    proactive: controller tracks qubit utilization

--- mqns/network/proactive/fw_nb.py
+++ mqns/network/proactive/fw_nb.py
@@ -1,5 +1,5 @@
 from collections.abc import Iterable
-from typing import TYPE_CHECKING, cast
+from typing import TYPE_CHECKING, cast, override
 
 from mqns.entity.memory import PathDirection
 from mqns.entity.qchannel import QuantumChannel
@@ -13,6 +13,10 @@ if TYPE_CHECKING:
     from mqns.network.proactive.forwarder import ProactiveForwarder
 
 
+def to_fib_erase_delay(t_cohere: Time) -> Time:
+    return t_cohere * 4
+
+
 class ProactiveForwarderNorthbound(ForwarderNorthbound):
     """
     Northbound interface to communicate with ``ProactiveRoutingController``.
@@ -20,10 +24,11 @@ class ProactiveForwarderNorthbound(ForwarderNorthbound):
 
     mux: MuxScheme
 
+    @override
     def install(self, fw: Forwarder) -> None:
         super().install(fw)
         self.mux = cast("ProactiveForwarder", fw).mux
-        self._fib_erase_delay = self.memory.t_cohere * 4
+        self._fib_erase_delay = to_fib_erase_delay(self.memory.t_cohere)
 
     @fw_control_cmd_handler("PATH_INSERT")
     def handle_path_insert(self, msg: PathInsertMsg) -> None:
